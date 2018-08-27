c9207ade296fa1489f2bc58ba7e4c725bd4ea563
ilmuonedata-chris <christian@ilmuonedata.com>
2018-08-27 15:31:29 +0700

    Add header to Landing Page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -4,6 +4,7 @@ export default class LandingPage extends Component {
   render() {
     return (
       <div>
+        <h2>This is Landing Page</h2>
         <p>Hey this is homepage</p>
         <p>A new change in branch...</p>
       </div>
